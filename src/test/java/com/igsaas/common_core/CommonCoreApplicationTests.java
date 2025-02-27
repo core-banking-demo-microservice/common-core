95a9c80c79809c34c5e1b9deec7acbe596db6b20
VaChanncheyleap <channcheyleap.va@igsaas.asia>
2025-02-27 23:57:48 +0700

    Add property files

---
+++ src/test/java/com/igsaas/common_core/CommonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igsaas.common_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
