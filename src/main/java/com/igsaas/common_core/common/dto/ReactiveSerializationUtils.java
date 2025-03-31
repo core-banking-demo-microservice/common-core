8dd188b8c778e8c599c6bc133f12300c33153576
VaChanncheyleap <channcheyleap.va@igsaas.asia>
2025-03-31 14:02:40 +0700

    Update pagination util

---
+++ src/main/java/com/igsaas/common_core/common/dto/ReactiveSerializationUtils.java
@@ -0,0 +1,34 @@
+package com.igsaas.common_core.common.dto;
+
+import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class ReactiveSerializationUtils {
+
+    public static <T> Mono<ResponseEntity<PaginatedResponse<T>>> paginateResponse(
+            Mono<List<T>> dataMono,
+            Mono<Long> totalCountMono,
+            int pageNumber,
+            int pageSize) {
+
+        return Mono.zip(dataMono, totalCountMono)
+                .map(tuple -> {
+                    List<T> data = tuple.getT1();
+                    long totalCount = tuple.getT2();
+                    int totalPage = (int) Math.ceil((double) totalCount / pageSize);
+
+                    final var pagination = new PaginatedResponse.Pagination(pageNumber + 1, pageSize, totalPage, totalCount);
+
+                    PaginatedResponse<T> response = new PaginatedResponse<>();
+                    response.setCode(200);
+                    response.setMessage("Fetched Data Successfully");
+                    response.setData(data);
+                    response.setPagination(pagination);
+
+                    return ResponseEntity.ok(response);
+                });
+    }
+}
+
