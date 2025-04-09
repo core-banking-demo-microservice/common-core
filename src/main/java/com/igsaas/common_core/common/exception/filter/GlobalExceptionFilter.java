e4ae261c0457f887adc46f4016a4a52a5c7b6bb9
VaChanncheyleap <channcheyleap.va@igsaas.asia>
2025-04-09 17:43:16 +0700

    Update common core exception handler

--- src/main/java/com/igsaas/common_core/common/exception/filter/GlobalExceptionFilter.java
+++ src/main/java/com/igsaas/common_core/common/exception/filter/GlobalExceptionFilter.java
@@ -6,14 +6,11 @@ import com.igsaas.common_core.common.dto.ErrorResponse;
 import com.igsaas.common_core.common.dto.HttpException;
 import com.igsaas.common_core.common.exception.DataIntegrityValidationException;
 import com.igsaas.common_core.common.exception.aabstract.*;
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -24,7 +21,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-@ControllerAdvice
 @Slf4j
 public class GlobalExceptionFilter {
     @ExceptionHandler(WebClientResponseException.class)
@@ -184,29 +180,5 @@ public class GlobalExceptionFilter {
         errorResponse.put("fieldErrors", fieldErrors);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    }
-
-    @ExceptionHandler(StatusRuntimeException.class)
-    public ResponseEntity<Object> handleGrpcStatusRuntimeException(final StatusRuntimeException ex) {
-        final var error = new HashMap<String, Object>();
-        error.put("errorCode", ex.getStatus().getCode().name());
-        error.put("errorPath", "gRPC");
-        error.put("errorMessage", ex.getStatus().getDescription());
-
-        HttpStatus httpStatus = mapGrpcStatusToHttp(ex.getStatus().getCode());
-        return new ResponseEntity<>(error, httpStatus);
-    }
-
-    private HttpStatus mapGrpcStatusToHttp(Status.Code grpcCode) {
-        return switch (grpcCode) {
-            case NOT_FOUND -> HttpStatus.NOT_FOUND;
-            case INVALID_ARGUMENT -> HttpStatus.BAD_REQUEST;
-            case ALREADY_EXISTS -> HttpStatus.CONFLICT;
-            case UNAUTHENTICATED -> HttpStatus.UNAUTHORIZED;
-            case PERMISSION_DENIED -> HttpStatus.FORBIDDEN;
-            case UNAVAILABLE -> HttpStatus.SERVICE_UNAVAILABLE;
-            case INTERNAL -> HttpStatus.INTERNAL_SERVER_ERROR;
-            default -> HttpStatus.BAD_GATEWAY;
-        };
     }
 }
---
+++ src/main/java/com/igsaas/common_core/common/exception/filter/GlobalExceptionGrpcFilter.java
@@ -0,0 +1,35 @@
+package com.igsaas.common_core.common.exception.filter;
+
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+
+public class GlobalExceptionGrpcFilter {
+    @ExceptionHandler(StatusRuntimeException.class)
+    public ResponseEntity<Object> handleGrpcStatusRuntimeException(final StatusRuntimeException ex) {
+        final var error = new HashMap<String, Object>();
+        error.put("errorCode", ex.getStatus().getCode().name());
+        error.put("errorPath", "gRPC");
+        error.put("errorMessage", ex.getStatus().getDescription());
+
+        HttpStatus httpStatus = mapGrpcStatusToHttp(ex.getStatus().getCode());
+        return new ResponseEntity<>(error, httpStatus);
+    }
+
+    private HttpStatus mapGrpcStatusToHttp(Status.Code grpcCode) {
+        return switch (grpcCode) {
+            case NOT_FOUND -> HttpStatus.NOT_FOUND;
+            case INVALID_ARGUMENT -> HttpStatus.BAD_REQUEST;
+            case ALREADY_EXISTS -> HttpStatus.CONFLICT;
+            case UNAUTHENTICATED -> HttpStatus.UNAUTHORIZED;
+            case PERMISSION_DENIED -> HttpStatus.FORBIDDEN;
+            case UNAVAILABLE -> HttpStatus.SERVICE_UNAVAILABLE;
+            case INTERNAL -> HttpStatus.INTERNAL_SERVER_ERROR;
+            default -> HttpStatus.BAD_GATEWAY;
+        };
+    }
+}
