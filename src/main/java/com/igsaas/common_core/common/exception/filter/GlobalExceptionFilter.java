ab8249ba8571230b2c1a250202591bcb7908cbda
VaChanncheyleap <channcheyleap.va@igsaas.asia>
2025-04-09 16:36:01 +0700

    Add GRPC exception handler

--- src/main/java/com/igsaas/common_core/common/exception/filter/GlobalExceptionFilter.java
+++ src/main/java/com/igsaas/common_core/common/exception/filter/GlobalExceptionFilter.java
@@ -6,6 +6,8 @@ import com.igsaas.common_core.common.dto.ErrorResponse;
 import com.igsaas.common_core.common.dto.HttpException;
 import com.igsaas.common_core.common.exception.DataIntegrityValidationException;
 import com.igsaas.common_core.common.exception.aabstract.*;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -182,5 +184,29 @@ public class GlobalExceptionFilter {
         errorResponse.put("fieldErrors", fieldErrors);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(StatusRuntimeException.class)
+    public ResponseEntity<Object> handleGrpcStatusRuntimeException(final StatusRuntimeException ex) {
+        final var error = new HashMap<String, Object>();
+        error.put("errorCode", ex.getStatus().getCode().name());
+        error.put("errorPath", "gRPC");
+        error.put("errorMessage", ex.getStatus().getDescription());
+
+        HttpStatus httpStatus = mapGrpcStatusToHttp(ex.getStatus().getCode());
+        return new ResponseEntity<>(error, httpStatus);
+    }
+
+    private HttpStatus mapGrpcStatusToHttp(Status.Code grpcCode) {
+        return switch (grpcCode) {
+            case NOT_FOUND -> HttpStatus.NOT_FOUND;
+            case INVALID_ARGUMENT -> HttpStatus.BAD_REQUEST;
+            case ALREADY_EXISTS -> HttpStatus.CONFLICT;
+            case UNAUTHENTICATED -> HttpStatus.UNAUTHORIZED;
+            case PERMISSION_DENIED -> HttpStatus.FORBIDDEN;
+            case UNAVAILABLE -> HttpStatus.SERVICE_UNAVAILABLE;
+            case INTERNAL -> HttpStatus.INTERNAL_SERVER_ERROR;
+            default -> HttpStatus.BAD_GATEWAY;
+        };
     }
 }
