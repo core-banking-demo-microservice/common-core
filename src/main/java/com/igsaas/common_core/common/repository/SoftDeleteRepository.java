617d0b5ee04efd916ba495e751c44e6dbcc55a4f
VaChanncheyleap <channcheyleap.va@igsaas.asia>
2025-04-17 21:45:22 +0700

    Add soft delete repo

---
+++ src/main/java/com/igsaas/common_core/common/repository/SoftDeleteRepository.java
@@ -0,0 +1,45 @@
+package com.igsaas.common_core.common.repository;
+
+import org.reactivestreams.Publisher;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.lang.NonNull;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@NoRepositoryBean
+public interface SoftDeleteRepository<T, ID> {
+    @NonNull
+    Mono<Long> count(Class<T> entityClass);
+
+    @NonNull
+    Mono<Boolean> existsById(@NonNull ID id, Class<T> entityClass);
+
+    @NonNull
+    Mono<T> findById(@NonNull ID id, Class<T> entityClass);
+
+    @NonNull
+    Flux<T> findAll(Class<T> entityClass);
+
+    @NonNull
+    Flux<T> findAllById(@NonNull Iterable<ID> iterable, Class<T> entityClass);
+
+    @NonNull
+    Flux<T> findAllById(@NonNull Publisher<ID> idPublisher, Class<T> entityClass);
+
+    @NonNull
+    Flux<T> findAll(@NonNull Sort sort, Class<T> entityClass);
+
+    @NonNull
+    @Transactional
+    Mono<Void> softDeleteAll(Class<T> entityClass);
+
+    @NonNull
+    Mono<Void> softDeleteAllById(@NonNull Iterable<? extends ID> ids, Class<T> entityClass);
+
+    @NonNull
+    @Transactional
+    Mono<Void> softDeleteById(@NonNull ID id, Class<T> entityClass);
+}
