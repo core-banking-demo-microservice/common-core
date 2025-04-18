2ec38fd40f80826be7b76ce1b39b2120c12d0075
VaChanncheyleap <channcheyleap.va@igsaas.asia>
2025-04-18 21:27:35 +0700

    Move to use constructur

--- src/main/java/com/igsaas/common_core/common/repository/impl/PaginationRepositoryImpl.java
+++ src/main/java/com/igsaas/common_core/common/repository/impl/PaginationRepositoryImpl.java
@@ -3,22 +3,23 @@ package com.igsaas.common_core.common.repository.impl;
 import com.igsaas.common_core.common.repository.AuditEntity;
 import com.igsaas.common_core.common.repository.BaseRepositoryService;
 import com.igsaas.common_core.common.repository.PaginationRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.relational.core.query.Criteria;
 import org.springframework.data.relational.core.query.Query;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Set;
 
-@Repository
-@RequiredArgsConstructor
 public class PaginationRepositoryImpl<T extends AuditEntity> implements PaginationRepository<T> {
     private final R2dbcEntityTemplate r2dbcEntityTemplate;
     private final Class<T> entityClass;
+
+    public PaginationRepositoryImpl(R2dbcEntityTemplate r2dbcEntityTemplate, Class<T> entityClass) {
+        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
+        this.entityClass = entityClass;
+    }
 
     protected Set<String> getSortableColumns() {
         return Set.of("created_date", "id", "created_by");
